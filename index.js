22a9d61687d716ee8515be97ce9c8745e2941d40
githinjisamson1 <githinjisamson148@gmail.com>
2023-09-18 03:50:11 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+function writeCards(names, eventName) {
+  let messages = [];
+  for (let i = 0; i < names.length; i++) {
+    messages.push(
+      `Thank you, ${names[i]}, for the wonderful ${eventName} gift!`
+    );
+  }
+  return messages;
+}
+
+function countDown(n) {
+  for (let i = n; i >= 0; i--) {
+    console.log(i);
+  }
+}
